ab0990504380a9a1b09d62d85b9d6502f0ca7f9b
maxdexter1401 <118183177+maxdexter1401@users.noreply.github.com>
2025-01-10 13:14:27 +0100

    Add files via upload
    Added a visual representation.
    Create() now makes a grid of the board without cars. 
    The Place() and Unplace() functions now place and remove* rectangles on the grid to represent the cars (with nice colours).
    Show() now saves a png of the current boardstate.
    
    *removing is done by placing a white rectangle. Let's hope we don't have to implement dark-mode.

--- code/classes/board_class.py
+++ code/classes/board_class.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as np
 from numpy import var
 import pandas as pd
 import os
@@ -9,18 +12,18 @@ class Board:
         """
         Load data from csv file
         """
-        
-        # Load csv file 
+
+        # Load csv file
         if file[-3:] != 'csv':
             raise ValueError("File should be .csv format")
 
-        self.file_path: str = "gameboards/" + file 
-        
+        self.file_path: str = "gameboards/" + file
+
         if os.path.exists(self.file_path):
             self.df = pd.read_csv(self.file_path)
         else:
             raise FileNotFoundError(f"The file '{self.file_path}' does not exist")
-        
+
         # Read car data and add to dictionary
         self.cars: dict[var] = {}
         nrows_df = self.df.shape[0]
@@ -35,10 +38,16 @@ class Board:
 
     def create(self):
         """
-        Create empty board 
+        Create empty board
         """
         self.N = int(self.df['N'].iloc[0])
         self.board = list()
+
+        # plot
+        self.board_figure, self.axes_figure = plt.subplots()
+        self.axes_figure.grid(linewidth=0.1)
+        plt.xlim(0, self.N + 2)
+        plt.ylim(-self.N - 2, 0)
 
         for row in range(self.N + 2):
             self.board.append(['.'] * (self.N + 2))
@@ -48,10 +57,18 @@ class Board:
             self.board[0][i] = "*"
             self.board[self.N + 1][i] = "*"
 
+            # paint black squares at border rows
+            self.axes_figure.add_patch(Rectangle((0, -i - 1), 1, 1, facecolor = 'black'))
+            self.axes_figure.add_patch(Rectangle((self.N + 1, -i - 1), 1, 1, facecolor = 'black'))
+
         # Create border columns
         for i in range(self.N + 2):
             self.board[i][0] = "*"
             self.board[i][self.N + 1] = "*"
+
+            # paint black squares at border columns
+            self.axes_figure.add_patch(Rectangle((i, -1), 1, 1, facecolor = 'black'))
+            self.axes_figure.add_patch(Rectangle((i, -self.N - 2), 1, 1, facecolor = 'black'))
 
         # Locate and place opening on the border
         if self.N % 2 == 0:
@@ -61,6 +78,9 @@ class Board:
 
         self.board[opening_index[0]][opening_index[1]] = '@'
 
+        # place white square at exit
+        self.axes_figure.add_patch(Rectangle((opening_index[1], -opening_index[0] - 1), 1, 1, facecolor = 'white'))
+
 
     def place(self, car: var) -> None:
         """
@@ -68,10 +88,21 @@ class Board:
         """
         for x, y in car.get_current_coördinates():
             self.board[x][y] = car.name
-    
+
+            # random colours based on car name
+            if car.name != 'X':
+                # 56 makes pretty colours :)
+                np.random.seed(ord(car.name) + 56)
+                self.axes_figure.add_patch(Rectangle((y, -x - 1), 1, 1, facecolor = (np.random.uniform(low=0, high=0.75), np.random.uniform(low=0.1, high=1), np.random.uniform(low=0.1, high=1))))
+
+            # red car, name 'X', always red
+            else:
+                self.axes_figure.add_patch(Rectangle((y, -x - 1), 1, 1, facecolor = (1, 0, 0)))
+
     def unplace(self, car: var):
         for x, y in car.get_current_coördinates():
             self.board[x][y] = '.'
+            self.axes_figure.add_patch(Rectangle((y, -x - 1), 1, 1, facecolor = 'white'))
 
     def fill(self):
         """
@@ -79,36 +110,36 @@ class Board:
         """
         for car in self.cars.values():
             self.place(car)
-        
+
 
     def move(self, car: var, direction: str, blocks: int) -> bool:
-        
+
         # Raise an error when invalid value is given
         if car.orientation == "H" and direction.upper() not in ("L", "R"):
             raise ValueError("Direction should be 'L' or 'R' for horizontal cars")
-            
+
         elif car.orientation == "V" and direction.upper() not in ("U", "D"):
             raise ValueError("Direction should be 'U' or 'D' for vertical cars")
-        
+
         # Check if move possible (all blocks are empty)
-        for i in range(blocks):    
+        for i in range(blocks):
             if direction.upper() == "L":
                 if self.board[car.row][car.current_coördinates[0][1] - range(1, blocks + 1)[i]] != ".":
                     return False
             if direction.upper() == "R":
                 if self.board[car.row][car.current_coördinates[car.length - 1][1] + range(1, blocks+ 1)[i]] != ".":
-                    return False 
+                    return False
             if direction.upper() == "U":
                 if self.board[car.current_coördinates[0][0] - range(1, blocks + 1)[i]][car.col] != ".":
-                    return False 
-            if direction.upper() == "D":    
+                    return False
+            if direction.upper() == "D":
                 if self.board[car.current_coördinates[car.length - 1][0] + range(1, blocks + 1)[i]][car.col] != ".":
                     return False
-        
+
         # first unplace car from postition
         self.unplace(car)
 
-        # move car 
+        # move car
         for i in range(car.length):
             if direction.upper() == "L":
                 car.current_coördinates[i][1] = car.current_coördinates[i][1] - blocks
@@ -126,5 +157,17 @@ class Board:
         """
         Function to let user see the board
         """
+        # saves a figure of the current board state
+        # need to think of a way to animate this
+        # plt.show() could work but fails in cs50
+        plt.savefig('Rush Hour Board classes')
         for row in self.board:
             print(row)
+
+board1 = Board("Rushhour6x6_1.csv")
+board1.create()
+board1.fill()
+board1.show()
+
+carA = board1.cars.get('A')
+board1.move(carA, 'L', 1)
