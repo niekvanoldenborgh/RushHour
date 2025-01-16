09ddde69bcfb42eb62d14899258e978b24a5b209
maxdexter1401 <118183177+maxdexter1401@users.noreply.github.com>
2025-01-16 15:58:10 +0100

    Add files via upload
    This main serves as a tutorial on how to call the random algorithm.
    (What does a main even need to have in it?)

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from code.classes.board_class import Board
 from code.algorithms.baseline import random_algorithm
 
 if __name__ == "__main__":
-    random_algorithm("Rushhour6x6_1.csv")
+    result_list = []
+    for x in range(0, 20):
+        result = random_algorithm("Rushhour6x6_3.csv", x)
+        result_list.append(result)
+        result = 0
+    print(result_list)
+
