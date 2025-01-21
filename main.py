06929451be3ee7848ff06a0fa539b19be05cadc9
maxdexter1401 <118183177+maxdexter1401@users.noreply.github.com>
2025-01-21 11:41:33 +0100

    Update main.py
    Removed the 'for' loop that was necessary for the previous version of the baseline.

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from code.classes.board_class import Board
 from code.algorithms.baseline import random_algorithm
 
 if __name__ == "__main__":
-    result_list = []
-    for x in range(0, 20):
-        result = random_algorithm("Rushhour6x6_3.csv", x)
-        result_list.append(result)
-        result = 0
-    print(result_list)
+    print(random_algorithm("Rushhour6x6_3.csv", 3))
+
 
