7f02b6d8ba2b10e8d514f2a8178f12d78c3f1aa4
maxdexter1401 <118183177+maxdexter1401@users.noreply.github.com>
2025-01-23 17:23:09 +0100

    Add files via upload
    New folder for individual functions.
    
    data_analysis.py takes a list of turns until victory and makes a histogram out of it.
    also prints average game length and shortest game.
    comes with seven lists 'all_turns_game_[1-7]' of the 1000x test runs of the baseline.
    
    game_visualizer.py has a function of 'replay_game' that takes a board, and a log of an already played game and replays it.
    it makes a visual representation of the game with a matplotlib grid.
    it will save a .png of the boardstate every single turn, allowing it to be animated.

---
+++ code/functions/game_visualizer.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from code.classes.board_class import Board
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as np
+
+def replay_game(board_file, log_file):
+
+    # read the log
+    df_log = pd.read_csv(log_file, sep=',')
+
+    # separate cars from moves
+    df_cars = df_log["car"]
+    df_moves = df_log["move"]
+    
+    # initialize board
+    board = Board(board_file)
+    board.create()
+    board.fill()
+    board.show()
+    print("")
+
+    # initialize grid
+    board_figure, axes_figure = plt.subplots()
+    axes_figure.grid(linewidth=0.1)
+    plt.xlim(0, len(board.board))
+    plt.ylim(-len(board.board), 0)
+    axes_figure.set_yticklabels([])
+    axes_figure.set_xticklabels([])
+    
+    # for every line in log file
+    for turn in range(0, len(df_cars)):
+
+        # select the car on current line and move it
+        selected_car = board.cars.get(df_cars[turn])
+        board.move(selected_car, df_moves[turn])
+        board.show()
+
+        # paint the grid
+        for y in range(0, len(board.board)):
+            for x in range(0, len(board.board)):
+
+                # '*' means wall, black rectangle
+                if board.board[y][x] == '*':
+                    axes_figure.add_patch(Rectangle((x, -y - 1), 1, 1, facecolor = 'black'))
+
+                # if not empty spot or exit
+                elif board.board[y][x] != '.' and board.board[y][x] != '@':
+                    
+                    # if not red car
+                    if board.board[y][x] != 'X':
+                        
+                        # character means car, random colour rectangle
+                        seed_sum = 0
+                        for character in board.board[y][x]:
+                            seed_sum = seed_sum + ord(character)
+                            # 56 makes pretty colours :)
+                            np.random.seed(seed_sum + 56)
+                        axes_figure.add_patch(Rectangle((x, -y - 1), 1, 1, facecolor = (np.random.uniform(low=0, high=0.75), np.random.uniform(low=0.1, high=1), np.random.uniform(low=0.1, high=1))))
+
+                    # 'X' means red car, red rectangle
+                    else:
+                        axes_figure.add_patch(Rectangle((x, -y - 1), 1, 1, facecolor = (1, 0, 0)))
+
+                # else is empty space, white rectangle
+                else:
+                    axes_figure.add_patch(Rectangle((x, -y - 1), 1, 1, facecolor = 'white'))
+                    
+
+        print("")
+        plt.savefig(f"game_frame_turn_{turn}.png")
