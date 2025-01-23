f95503be9af41d2ac8576fbbfa5ea296254aa731
maxdexter1401 <118183177+maxdexter1401@users.noreply.github.com>
2025-01-23 17:25:01 +0100

    Add files via upload
    Latest main, now with the new functions of game_visualizer and data_analysis included in the imports

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from code.classes.board_class import Board
 from code.algorithms.baseline import random_algorithm
+from code.algorithms.random_exp import random_algorithm_exp
+from code.functions.game_visualizer import replay_game
+from code.functions.data_analysis import *
 
 if __name__ == "__main__":
-    print(random_algorithm("Rushhour6x6_3.csv", 3))
-
-
+    # print(random_algorithm("Rushhour6x6_1.csv", 1))
+    # print(random_algorithm_exp("Rushhour6x6_1.csv", 99))
+    # replay_game("Rushhour6x6_easy.csv", "fake_log.csv")
+    # data_analysis(all_turns_game_1)
+    pass
