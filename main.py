ae54cb52fea9c6aa6484682dce12c31471c9f49f
Niek van Oldenborgh <98456973+niekvanoldenborgh@users.noreply.github.com>
2025-01-28 10:02:40 +0100

    added time limit for depth first in main.py

--- main.py
+++ main.py
@@ -27,16 +27,47 @@ if __name__ == "__main__":
         board_file = "Rushhour12x12_7.csv"
     else:
         print("Invalid board choice. Please choose a number to play.")
-
+        
+    n_times_input = int(input("How many times would you like to play? \n"))
+    
+    print("Would you like to add a time limit?\n 1. No time limit\n 2. Only time limit per game\n 3. Only overall time limit\n 4. Both overall time limit and time limit per game\n")
+    time_limit_input = input("Please choose a number:\n")
+    game_time_limit_ind = False
+    overall_time_limit_ind = False
+    
+    if time_limit_input == '1':
+        pass
+    elif time_limit_input == '2':
+        game_time_limit = int(input("Time limit in seconds:\n"))
+        game_time_limit_ind = True
+    elif time_limit_input == '3':
+        overall_time_limit = int(input("Time limit in seconds: \n"))
+        overall_time_limit_ind = True
+    elif time_limit_input == '4':
+        game_time_limit = int(input("Time limit per game in seconds:\n"))
+        overall_time_limit = int(input("Overall time limit in seconds: \n"))
+        game_time_limit_ind = True
+        overall_time_limit_ind = True
 
     print("Choose an algorithm to solve the board:\n 1. Random search\n 2. Breadth first search\n 3. Depth first search")
-    algorithm_input = input("Please choose a number: ")
+    algorithm_input = input("Please choose a number:\n")
 
     if algorithm_input == "1":
         random_algorithm(board_file, 1)
     elif algorithm_input == "2":
         breadth_first_search(board_file, 1)
+    
     elif algorithm_input == "3":
-        depth_first_search(board_file, 1)
+        
+        if game_time_limit_ind and overall_time_limit_ind:
+            depth_first_search(board_file, n_times_input, game_time_limit, overall_time_limit)
+        elif game_time_limit_ind and not overall_time_limit_ind:
+            depth_first_search(board_file, n_times_input, max_time_game = game_time_limit)
+        elif not game_time_limit_ind and overall_time_limit_ind:
+            depth_first_search(board_file, n_times_input, max_time_overall = overall_time_limit)
+        else:
+            depth_first_search(board_file, n_times_input)
+        
     else: 
         print("Invalid choice. Please choose a number.")
+
