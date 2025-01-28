2f756d06b6a526185954e82f2f46a067646b2a09
Niek van Oldenborgh <98456973+niekvanoldenborgh@users.noreply.github.com>
2025-01-28 18:26:23 +0100

    main accepteert alleen correcte waardes

--- main.py
+++ main.py
@@ -12,9 +12,15 @@ import time
 from code.functions.data_analysis import *
 
 if __name__ == "__main__":
+    
+    # Give small welcome and ask what board to play
     print("Welcome to Rush Hour!\nWhat game would you like to play?\n 1. board 1 (6x6)\n 2. board 2 (6x6)\n 3. board 3 (6x6)\n 4. board 4 (9x9)\n 5. board 5 (9x9)\n 6. board 6 (9x9)\n 7. board 7 (12x12)\n")
     board_input = input("Please choose a number: \n")
-
+    
+    while board_input not in "1234567":
+        print("Invalid board choice.")
+        board_input = input("Please choose a number: \n")
+        
     if board_input == "1":
         board_file = "Rushhour6x6_1.csv"
     elif board_input == "2":
@@ -29,30 +35,61 @@ if __name__ == "__main__":
         board_file = "Rushhour9x9_6.csv"
     elif board_input == "7":
         board_file = "Rushhour12x12_7.csv"
-    else:
-        print("Invalid board choice. Please choose a number to play.")
-        
-    n_times_input = int(input("How many times would you like to play? \n"))
+
+    # Ask user how many times to simulate the game    
+    n_times_input = input("How many times would you like to play? \n")
     
+    while not n_times_input.isdigit():
+        print("Invalid choice. Please choose a number.")
+        n_times_input = input("How many times would you like to play? \n")
+    
+    n_times_input = int(n_times_input)
+    
+    # As user for a time limit
     print("Would you like to add a time limit?\n 1. No time limit\n 2. Only time limit per game\n 3. Only overall time limit\n 4. Both overall time limit and time limit per game\n")
     time_limit_input = input("Please choose a number:\n")
     game_time_limit_ind = False
     overall_time_limit_ind = False
     
+    while time_limit_input not in "1234":
+        print("Invalid choice.")
+        time_limit_input = input("Please choose a number:\n")
+    
     if time_limit_input == '1':
         pass
     elif time_limit_input == '2':
-        game_time_limit = int(input("Time limit in seconds:\n"))
-        game_time_limit_ind = True
+        game_time_limit = input("Time limit in seconds:\n")
+        while not game_time_limit.isdigit():
+            print("Please choose a valid int.")
+            game_time_limit = input("Time limit in seconds:\n")
+        
+        game_time_limit = float(game_time_limit)
+        game_time_limit_ind = True       
     elif time_limit_input == '3':
-        overall_time_limit = int(input("Time limit in seconds: \n"))
+        overall_time_limit = input("Time limit in seconds: \n")
+        while not overall_time_limit.isdigit():
+            print("Please choose a valid int.")
+            overall_time_limit = input("Time limit in seconds:\n")
+        
+        overall_time_limit = float(overall_time_limit)
         overall_time_limit_ind = True
     elif time_limit_input == '4':
-        game_time_limit = int(input("Time limit per game in seconds:\n"))
-        overall_time_limit = int(input("Overall time limit in seconds: \n"))
+        game_time_limit = input("Time limit per game in seconds:\n")
+        while not game_time_limit.isdigit():
+            print("Please choose a valid int.")
+            game_time_limit = input("Time limit per game in seconds:\n")
+            
+        overall_time_limit = input("Overall time limit in seconds: \n")
+        while not overall_time_limit.isdigit():
+            print("Please choose a valid int.")
+            overall_time_limit = input("Overall time limit in seconds:\n")
+        
+        game_time_limit = int(game_time_limit)
+        overall_time_limit = int(overall_time_limit)
         game_time_limit_ind = True
         overall_time_limit_ind = True
-
+    
+    # Ask user which algorthm to use
     print("Choose an algorithm to solve the board:\n 1. Random search\n 2. Breadth first search\n 3. Depth first search")
     algorithm_input = input("Please choose a number:\n")
 
@@ -92,4 +129,5 @@ if __name__ == "__main__":
         
     else: 
         print("Invalid choice. Please choose a number.")
+
 
