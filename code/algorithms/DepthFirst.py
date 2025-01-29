6b0a1b71069c5e83f3099e0a27ed62c950a78f8d
Niek van Oldenborgh <98456973+niekvanoldenborgh@users.noreply.github.com>
2025-01-29 09:12:42 +0100

    added comments and cleaned code

--- code/algorithms/DepthFirst.py
+++ code/algorithms/DepthFirst.py
@@ -18,7 +18,7 @@ class DepthFirst:
         self.board.create()
         self.board.fill()
 
-        # initial state
+        # initial state of key variables
         self.states_stack = []
         self.visited = set()
         self.visited.add(tuple(sum(self.board.board, [])))
@@ -28,7 +28,7 @@ class DepthFirst:
     
     def get_neighbours(self):
         """
-        Function that generates all possible 1-block moves
+        Method that generates all possible 1-block moves per car in the current state
         """
         moves = [1, -1]
         neighbours = []
@@ -38,7 +38,7 @@ class DepthFirst:
                 if self.board.is_valid(car, move):
                     neighbours.append({'car': car.name, 'move': move})
         
-        # shuffle neighbours so algorithm does not always take same
+        # shuffle neighbours so algorithm does not always take the same path
         random.shuffle(neighbours)
 
         return neighbours
@@ -46,12 +46,12 @@ class DepthFirst:
     def get_neighbour_states(self):
         
         """
-         Function that generates all neighbour states based on possible moves and adds them to the stack
+         Method that generates all neighbour states based on possible moves and adds them to the stack if not already visited.
         """
         
         neighbours = self.get_neighbours()
 
-        # move to neighbour, copy move, save the board, go back to initial state
+        # move to neighbour, copy board state after move, add the copy to the stack, go back to initial state
         for neighbour in neighbours:
             car = self.board.cars.get(neighbour['car'])
             move = neighbour['move']
@@ -59,20 +59,28 @@ class DepthFirst:
             self.board.move(car, move)
             board_tmp = copy.deepcopy(self.board.board)
 
-            # add to stack if not visited
+            # add board and path to stack if not visited
             if tuple(sum(board_tmp, [])) not in self.visited:
                 current_coördinates = copy.deepcopy(self.board.get_car_coördinates())
                 self.states_stack.append(current_coördinates)
                 self.path_stack.append(self.path + [(car.name, move)]) 
                 self.visited.add(tuple(sum(board_tmp, [])))
-
+            
+            # unmove to go to initial state
             self.board.move(car, -1*move)           
     
     def next_state(self):
+        """
+        Method that sets the new state of the board. Returns True if current state is the goal state, 
+        otherwise it sets the new state and returns false.
+        """
+        
+        # if goal state, return True
         if self.board.is_won():
             print("Exit found!")
             return True
         
+        # go to next state in stack
         if self.states_stack:
             new_coördinates = self.states_stack.pop()
             self.path = self.path_stack.pop()
@@ -81,57 +89,80 @@ class DepthFirst:
         return False
     
     def get_depth(self):
+        """
+        Returns depth level of the current state (number of moves before getting to this state)
+        """
         return len(self.path)
         
     def get_path_as_csv(self, filename = "output.csv"):
          """
-         Return moves needed to solve puzzle as csv
+         Returns moves needed to solve rushhour game as csv in format compatible for check50.
          """
-        # Make list of dictionaries that can be transormed to csv
+        # make list of dictionaries that can be transormed to csv
          logbook = []
          for car, move in self.path:
              logbook.append({"car": car, 'move': move})
         
-        # Create csv file
+        # create csv file
          df = pd.DataFrame(logbook)
          df.to_csv(filename, index = False)
          print(f"Logbook to saved to {filename}")
     
     def dfs(self, game, max_time_game = float('inf')):
+        """
+        MNethod that executes depth first search for a single game.
+        """
+        
+        print(f"Solving game {game}...")
+        
+        # initialize variables
         start_time = time.time()
         self.get_neighbour_states()
         success = self.next_state()
         results = []
-
+        
+        # while goal state not reached, go to next state
         while not success:
-            # check if time limit is reached
+            
+            # check the time constraint per game
             if time.time() - start_time >= max_time_game:
                 print(f"Game time limit of {max_time_game} seconds reached. Terminating search.")
                 return None
             
             self.get_neighbour_states()
             success = self.next_state()
-            
+        
+        # print and return results
         moves = len(self.path)
-        print(f"Moves: {moves}, Time: {time.time() - start_time:.2f} seconds, {self.board.show()}, Game: {game}")
+        self.get_path_as_csv()
+        board.show()
+        print(f"Moves: {moves}, Time: {time.time() - start_time:.2f} seconds, Game: {game}")
         results.append({"moves": moves, "time (sec)": round(time.time() - start_time, 2), "game": game})
         
         return results
 
 def depth_first_search(boardname, number_of_games, max_time_game = float('inf'), max_time_overall = float('inf')):
+    """
+    Function (outside of DepthFisrt class) that runs algorithm on multiple games.
+    """
+    
+    # initialize variables
     start_time = time.time()
     game = 1
     results = []
     
-    
+    # For each game, run the algorithm
     while game <= number_of_games:
+        
+        # check the overall time constraint
         if time.time() - start_time >= max_time_overall:
             print(f"Overall time limit of {max_time_overall} seconds reached. Terminating search.")
-            return results
-        
-           
+            return results    
+         
         rush = DepthFirst(boardname)
         game_results = rush.dfs(game, max_time_game)
+        
+        # small if-statement that prevents None results to be added to results file
         if game_results is not None:
             results.extend(game_results)
             
